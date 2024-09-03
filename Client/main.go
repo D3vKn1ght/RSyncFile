449fbffedac9c854b864a203bae04003dca25a27
d3vkn1ght <d3vkn1ghtz@gmail.com>
2024-09-03 10:50:51 +0700

    update client

--- Client/main.go
+++ Client/main.go
@@ -1,202 +1,179 @@
 package main
 
 import (
-	"compress/gzip"
-	"encoding/json"
-	"fmt"
-	"io"
-	"io/ioutil"
-	"log"
-	"net/http"
-	"os"
-	"path/filepath"
-	"strings"
-	"time"
+    "bytes"
+    "compress/gzip"
+    "encoding/json"
+    "fmt"
+    "io"
+    "mime/multipart"
+    "net/http"
+    "os"
+    "path/filepath"
+    "strings"
+    "time"
 )
 
 type Config struct {
-	URL           string `json:"url"`
-	FolderToWatch string `json:"folder_to_watch"`
+    URL           string `json:"url"`
+    FolderToWatch string `json:"folder_to_watch"`
 }
 
 func loadConfig(configPath string) (Config, error) {
-	var config Config
+    var config Config
+    file, err := os.Open(configPath)
+    if err != nil {
+        return config, err
+    }
+    defer file.Close()
 
-	file, err := os.Open(configPath)
-	if err != nil {
-		return config, err
-	}
-	defer file.Close()
+    bytes, err := io.ReadAll(file)
+    if err != nil {
+        return config, err
+    }
 
-	bytes, err := ioutil.ReadAll(file)
-	if err != nil {
-		return config, err
-	}
-
-	err = json.Unmarshal(bytes, &config)
-	return config, err
+    err = json.Unmarshal(bytes, &config)
+    return config, err
 }
 
 func convertToLinuxPath(winPath string) string {
-	return strings.ReplaceAll(winPath, "\\", "/")
+    return strings.ReplaceAll(winPath, "\\", "/")
 }
 
-func checkForChanges(folder string, lastModTimes map[string]time.Time, serverURL string) {
-	files, _ := ioutil.ReadDir(folder)
-	seenFiles := make(map[string]bool)
+func checkForChanges(folderToWatch string, folder string, lastModTimes map[string]time.Time, serverURL string) {
+    files, _ := os.ReadDir(folder)
 
-	for _, file := range files {
-		if !file.IsDir() {
-			filePath := filepath.Join(folder, file.Name())
-			fileModTime := file.ModTime()
-			seenFiles[filePath] = true
+    // Duyệt qua các file hiện tại trong thư mục
+    for _, file := range files {
+        if !file.IsDir() {
+            filePath := filepath.Join(folder, file.Name())
+            info, err := os.Stat(filePath)
+            if err != nil {
+                continue
+            }
+            fileModTime := info.ModTime()
 
-			if lastModTimes[filePath].Before(fileModTime) {
-				lastModTimes[filePath] = fileModTime
-				if err := createTempAndUpload(filePath, folder, serverURL); err != nil {
-					log.Printf("Failed to upload file %s: %v\n", filePath, err)
-				}
-			}
-		} else {
-			checkForChanges(filepath.Join(folder, file.Name()), lastModTimes, serverURL)
-		}
-	}
+            if lastModTime, exists := lastModTimes[filePath]; !exists || lastModTime != fileModTime {
+                lastModTimes[filePath] = fileModTime
+                fmt.Printf("Detected change in folder: %s\n", folder)
+                fmt.Printf("File: %s, Last Modified: %s\n", file.Name(), fileModTime)
+                err := gzipAndUploadFile(folderToWatch, filePath, folder, serverURL)
+                if err != nil {
+                    fmt.Printf("Error uploading file %s: %v\n", file.Name(), err)
+                    continue
+                }
+            }
+        } else {
+            checkForChanges(folderToWatch, filepath.Join(folder, file.Name()), lastModTimes, serverURL)
+        }
+    }
 
-	for filePath := range lastModTimes {
-		if !seenFiles[filePath] {
-			if err := deleteFileOnServer(filePath, folder, serverURL); err != nil {
-				log.Printf("Failed to delete file %s on server: %v\n", filePath, err)
-			}
-			delete(lastModTimes, filePath)
-		}
-	}
+    for filePath := range lastModTimes {
+        if _, err := os.Stat(filePath); os.IsNotExist(err) {
+            fmt.Printf("Detected not seen: %s\n", filePath)
+            err := deleteFileOnServer(folderToWatch, filePath, folder, serverURL)
+            if err == nil {
+                delete(lastModTimes, filePath)
+            }
+        }
+    }
 }
 
-func createTempAndUpload(filePath, rootFolder, serverURL string) error {
-	tmpFilePath := filePath + ".tmp"
+func gzipAndUploadFile(folder,filePath, rootFolder, serverURL string) error {
 
-	if err := copyFile(filePath, tmpFilePath); err != nil {
-		return err
-	}
-	defer os.Remove(tmpFilePath)
+    relativePath := strings.TrimLeft(filePath, folder)
+    relativePath = convertToLinuxPath(relativePath) // Chuyển đổi sang dạng Linux
 
-	relativePath := strings.TrimPrefix(tmpFilePath, rootFolder)
-	relativePath = convertToLinuxPath(relativePath)
+    file, err := os.Open(filePath)
+    if err != nil {
+        return err
+    }
+    defer file.Close()
 
-	if err := gzipAndUploadFile(tmpFilePath, relativePath, serverURL); err != nil {
-		return err
-	}
+    var gzipBuffer bytes.Buffer
+    gzipWriter := gzip.NewWriter(&gzipBuffer)
+    if _, err := io.Copy(gzipWriter, file); err != nil {
+        return err
+    }
+    gzipWriter.Close()
 
-	os.Rename(tmpFilePath, filePath)
-	return nil
+    body := &bytes.Buffer{}
+    writer := multipart.NewWriter(body)
+    part, err := writer.CreateFormFile("file", filepath.Base(filePath)+".gz")
+    if err != nil {
+        return err
+    }
+    _, err = io.Copy(part, &gzipBuffer)
+    if err != nil {
+        return err
+    }
+
+    writer.Close()
+
+    request, err := http.NewRequest("POST", serverURL+"/uploadfile/", body)
+    if err != nil {
+        return err
+    }
+
+    request.Header.Set("Content-Type", writer.FormDataContentType())
+    request.Header.Set("X-Filename", relativePath+".gz")
+
+    client := &http.Client{}
+    response, err := client.Do(request)
+    if err != nil {
+        return err
+    }
+    defer response.Body.Close()
+
+    if response.StatusCode != http.StatusOK {
+        return fmt.Errorf("upload failed with status: %s", response.Status)
+    }
+
+    fmt.Printf("File %s uploaded successfully to %s\n", relativePath, serverURL)
+    return nil
 }
 
-func copyFile(src, dst string) error {
-	sourceFile, err := os.Open(src)
-	if err != nil {
-		return err
-	}
-	defer sourceFile.Close()
 
-	destinationFile, err := os.Create(dst)
-	if err != nil {
-		return err
-	}
-	defer destinationFile.Close()
+func deleteFileOnServer(folderToWatch,filePath, rootFolder, serverURL string) error {
+    relativePath := strings.TrimLeft(filePath, folderToWatch)
+    relativePath = convertToLinuxPath(relativePath)
 
-	_, err = io.Copy(destinationFile, sourceFile)
-	return err
-}
+    request, err := http.NewRequest("DELETE", serverURL+"/deletefile/", nil)
+    if err != nil {
+        return err
+    }
 
-func gzipAndUploadFile(filePath, relativePath, serverURL string) error {
-	file, err := os.Open(filePath)
-	if err != nil {
-		return err
-	}
-	defer file.Close()
+    request.Header.Set("filename", relativePath)
 
-	gzipFilePath := filePath + ".gz"
-	gzipFile, err := os.Create(gzipFilePath)
-	if err != nil {
-		return err
-	}
-	defer os.Remove(gzipFilePath)
-	defer gzipFile.Close()
+    client := &http.Client{}
+    response, err := client.Do(request)
+    if err != nil {
+        return err
+    }
+    defer response.Body.Close()
 
-	gzipWriter := gzip.NewWriter(gzipFile)
-	if _, err := io.Copy(gzipWriter, file); err != nil {
-		return err
-	}
-	gzipWriter.Close()
+    if response.StatusCode != http.StatusOK {
+        return fmt.Errorf("delete failed with status: %s", response.Status)
+    }
 
-	gzipFile, err = os.Open(gzipFilePath)
-	if err != nil {
-		return err
-	}
-	defer gzipFile.Close()
-
-	request, err := http.NewRequest("POST", serverURL+"/uploadfile/", gzipFile)
-	if err != nil {
-		return err
-	}
-
-	request.Header.Set("Content-Type", "application/gzip")
-	request.Header.Set("filename", relativePath)
-
-	client := &http.Client{}
-	response, err := client.Do(request)
-	if err != nil {
-		return err
-	}
-	defer response.Body.Close()
-
-	if response.StatusCode != http.StatusOK {
-		return fmt.Errorf("upload failed with status: %s", response.Status)
-	}
-
-	return nil
-}
-
-func deleteFileOnServer(filePath, rootFolder, serverURL string) error {
-	relativePath := strings.TrimPrefix(filePath, rootFolder)
-	relativePath = convertToLinuxPath(relativePath)
-
-	request, err := http.NewRequest("DELETE", serverURL+"/deletefile/", nil)
-	if err != nil {
-		return err
-	}
-
-	request.Header.Set("filename", relativePath)
-
-	client := &http.Client{}
-	response, err := client.Do(request)
-	if err != nil {
-		return err
-	}
-	defer response.Body.Close()
-
-	if response.StatusCode != http.StatusOK {
-		return fmt.Errorf("delete failed with status: %s", response.Status)
-	}
-
-	return nil
+    fmt.Printf("File %s deleted successfully from server\n", relativePath)  // Print thông tin file bị xóa thành công
+    return nil
 }
 
 func main() {
-	configPath := "config.json"
+    configPath := "config.json"
+    config, err := loadConfig(configPath)
+    if err != nil {
+        fmt.Printf("Failed to load config: %v\n", err)
+        return
+    }
 
-	config, err := loadConfig(configPath)
-	if err != nil {
-		log.Fatalf("Failed to load config: %v", err)
-	}
+    folderToWatch := config.FolderToWatch
+    serverURL := config.URL
+    lastModTimes := make(map[string]time.Time)
 
-	folderToWatch := config.FolderToWatch
-	serverURL := config.URL
-
-	lastModTimes := make(map[string]time.Time)
-
-	for {
-		checkForChanges(folderToWatch, lastModTimes, serverURL)
-		time.Sleep(10 * time.Second)
-	}
+    for {
+        checkForChanges(folderToWatch,folderToWatch, lastModTimes, serverURL)
+        time.Sleep(10 * time.Second)
+    }
 }
